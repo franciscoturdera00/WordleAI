3ca97c8961b903fdc10935438f5f7f8288aeacb6
= <=>
2022-03-20 14:51:27 -0400

    added coverage

--- tests/test_environment.py
+++
@@ -1,8 +0,0 @@
-from strategies.random_strategy import RandomStrategy
-
-# An environment to free-test while in development
-
-# strat = RandomStrategy("word_banks/test.txt", 5)
-# print(strat.guess())
-# print(strat.guess())
-# print(strat.guess())
