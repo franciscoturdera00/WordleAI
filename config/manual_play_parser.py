3c4fede13a5938b3450725a09e8b2fa4595fc93d
= <=>
2022-03-22 00:40:05 -0400

    small fix

--- config/manual_play_parser.py
+++ config/manual_play_parser.py
@@ -3,8 +3,10 @@ import argparse
 
 def manual_parser():
     parser = argparse.ArgumentParser(description='Play Wordle')
-    parser.add_argument('-a', '--attempts', dest='attempts', default=6, help='Number of attempts the player receives')
-    parser.add_argument('-wb', '--word-bank', dest='word_bank', default=None, help='Word Bank to be used for game')
-    parser.add_argument('-sb', '--secret-bank', dest='secret_bank', default=None,
+    parser.add_argument('-a', '--attempts', dest='attempts', default=6, type=int,
+                        help='Number of attempts the player receives')
+    parser.add_argument('-wb', '--word-bank', dest='word_bank', default=None, type=str,
+                        help='Word Bank to be used for game')
+    parser.add_argument('-sb', '--secret-bank', dest='secret_bank', default=None, type=str,
                         help='File Path for auxiliary allowed guesses')
     return parser.parse_args()
